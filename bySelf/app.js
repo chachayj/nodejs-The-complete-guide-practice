c2e644d5f1a827d9f2a5db041e528ce797398da6
yjcha <yjcha010@gmail.com>
2019-04-08 00:15:56 +0900

    connecting by mongoose and creating collection, add product

--- bySelf/app.js
+++ bySelf/app.js
@@ -2,10 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -18,20 +19,27 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('5ca9e5d8d23a0a2cfcfad643')
-  .then(user => {
-    req.user = new User(user.name, user.email, user.cart, user._id);
-    next();
-  })
-  .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('5ca9e5d8d23a0a2cfcfad643')
+//   .then(user => {
+//     req.user = new User(user.name, user.email, user.cart, user._id);
+//     next();
+//   })
+//   .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    'mongodb+srv://chacha:merlot@cluster0-3zuzi.mongodb.net/shop?retryWrites=true', { useNewUrlParser: true }
+  )
+  .then(result => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
